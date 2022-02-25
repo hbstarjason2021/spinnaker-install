2b960ecd1511c6662091558f5ad38c07a0ed2813
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 02:18:44 +0800

    Update setup-spinnaker.sh

--- setup-spinnaker.sh
+++ setup-spinnaker.sh
@@ -6,13 +6,13 @@ SPINNAKER_VERSION=1.25.7
 
 sudo hal config version edit --version ${SPINNAKER_VERSION}
 
-hal config provider kubernetes enable
-hal config provider kubernetes account add my-k8s \
+sudo hal config provider kubernetes enable
+sudo hal config provider kubernetes account add my-k8s \
     --provider-version v2 \
     --context $(kubectl config current-context)
         
-hal config deploy edit --type=distributed --account-name my-k8s
+sudo hal config deploy edit --type=distributed --account-name my-k8s
 
-chmod 777 ~/.kube && chmod 777  ~/.kube/config
+sudo chmod 777 ~/.kube && chmod 777  ~/.kube/config
 
-hal deploy apply
+sudo hal deploy apply
