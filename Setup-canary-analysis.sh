98325a195795d68f9c67ca7c1695648192899d4d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 21:40:59 +0800

    Create Setup-canary-analysis.sh

---
+++ Setup-canary-analysis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+### https://spinnaker.io/docs/setup/other_config/canary/
+
+PROMETHEUS_ACCOUNT="prometheus-account"
+PROMETHEUS_URL="http://prometheus-prometheus.monitoring:9090"
+
+hal config canary enable
+hal config canary edit --show-all-configs-enabled false
+hal config canary edit --default-metrics-store prometheus
+hal config canary edit --default-metrics-account $PROMETHEUS_ACCOUNT
+
+hal config canary prometheus enable
+hal config canary prometheus account add $PROMETHEUS_ACCOUNT --base-url http://$PROMETHEUS_URL
+
+hal config canary prometheus account list
