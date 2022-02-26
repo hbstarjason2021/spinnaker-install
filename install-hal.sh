0cc80f429e2653bc5b60e86477b74e8d1d7ec01c
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 11:14:15 +0800

    Update install-hal.sh

--- install-hal.sh
+++ install-hal.sh
@@ -8,7 +8,7 @@ else
   echo "=== hal doesn't exist, installing"
   sudo apt install -y default-jre
   curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
-  #sudo useradd -m zhang
+  sudo useradd -m zhang
   sudo bash InstallHalyard.sh --user zhang -y
   echo "hal version: $(hal -v)"
 fi
