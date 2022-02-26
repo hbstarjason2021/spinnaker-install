81ac7e97d63467c9b8923a1a2eaa4d29ce33ffd0
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 13:08:04 +0800

    Update install-hal.sh

--- install-hal.sh
+++ install-hal.sh
@@ -3,9 +3,11 @@
 set -euo pipefail
 
 if (command -v hal >/dev/null 2>&1) then
-  echo "=== hal exists, skipping"
+  echo -e "\033[31m hal exists, skipping \033[0m"
+  ##echo "=== hal exists, skipping"
 else
-  echo "=== hal doesn't exist, installing"
+  echo -e "\033[32m hal doesn't exist, installing \033[0m"
+  ## echo "=== hal doesn't exist, installing"
   sudo apt install -y default-jre
   curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
   sudo useradd -m zhang
