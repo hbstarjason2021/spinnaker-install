bb18f651fbff20f3cd4d5684576199b47d40b023
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 10:49:46 +0800

    Update setup-spinnaker.sh

--- setup-spinnaker.sh
+++ setup-spinnaker.sh
@@ -3,19 +3,27 @@
 set -e
 
 SPINNAKER_VERSION=1.25.7
+ACCOUNT_NAME=my-k8s
 
-sudo hal config version edit --version ${SPINNAKER_VERSION}
+hal config provider kubernetes enable
 
-sudo hal config provider kubernetes enable
-sudo hal config provider kubernetes account add my-k8s \
-    --provider-version v2 \
-    --context $(kubectl config current-context)
-        
-sudo hal config deploy edit --type=distributed --account-name my-k8s
+if [ -z "$(hal config provider kubernetes account list | grep ${ACCOUNT_NAME})" ]; then
+   CONTEXT=$(kubectl config current-context)
+   echo "=== Add kubernetes account ${ACCOUNT_NAME} with context ${CONTEXT}"
+   hal config provider kubernetes account add ${ACCOUNT_NAME} \
+      --provider-version v2 \
+      --context ${CONTEXT}
+else
+  echo "=== kubernetes account ${ACCOUNT_NAME} exists"
+fi
+
+hal config version edit --version ${SPINNAKER_VERSION}
+       
+hal config deploy edit --type=distributed --account-name ${ACCOUNT_NAME}
+
 
 sudo chmod 777 ~/.kube && chmod 777  ~/.kube/config
 
-mkdir -p /home/zhang/.kube
-sudo cp -i /root/.kube/config /home/zhang/.kube/config
+#mkdir -p /home/zhang/.kube
+#cp -i /root/.kube/config /home/zhang/.kube/config
 
-sudo hal deploy apply
