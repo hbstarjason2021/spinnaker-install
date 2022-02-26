5e3d569b29e8fc3d22ca66b176e9aeec08247956
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-02-26 16:25:11 +0800

    Create upgrade-master.sh

---
+++ upgrade-master.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+### https://kubernetes.io/zh/docs/tasks/administer-cluster/kubeadm/kubeadm-upgrade/
+
+sudo apt-mark unhold kubeadm && \
+sudo apt-get update && apt-get install -y kubeadm=1.19.4-00  && \
+sudo apt-mark hold kubeadm 
+
+kubeadm version
+
+### sudo kubectl drain kmaster --ignore-daemonsets --delete-local-data
+
+kubeadm upgrade plan
+
+### kubeadm upgrade plan --config /etc/kubernetes/kubeadm.yaml
+
+sudo kubeadm upgrade apply v1.19.4
+
+
+sudo apt-mark unhold kubelet kubectl && \
+sudo apt-get update && sudo apt-get install -y kubelet=1.19.4-00 kubectl=1.19.4-00 && \
+sudo apt-mark hold kubelet kubectl
+
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
+
+## sudo kubectl uncordon master
+
+kubectl version 
+
+kubectl get node 
