2c61879b0335847629c417116a9af0999f26b71f
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-03-08 15:20:49 +0800

    Update install-hal.sh

--- install-hal.sh
+++ install-hal.sh
@@ -11,15 +11,18 @@ white="\033[0m"
 USER=zhang
 
 if (command -v hal >/dev/null 2>&1) then
-  echo -e "\033[31m hal exists, skipping \033[0m"
-  ##echo "=== hal exists, skipping"
+  echo -e "$red hal exists, skipping $white"
+  ## echo -e "\033[31m hal exists, skipping \033[0m"
+  ## echo "=== hal exists, skipping"
 else
-  echo -e "\033[32m hal doesn't exist, installing \033[0m"
+  echo -e "$green hal doesn't exist, installing $white"
+  ## echo -e "\033[32m hal doesn't exist, installing \033[0m"
   ## echo "=== hal doesn't exist, installing"
   sudo apt install -y default-jre
   curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
   sudo useradd -m zhang
   sudo bash InstallHalyard.sh --user ${USER} -y
-  echo -e "\033[32m hal version: $(hal -v) \033[0m"
+  echo -e "$yellow hal version: $(hal -v) $white"
+  ## echo -e "\033[32m hal version: $(hal -v) \033[0m"
   ## echo "hal version: $(hal -v)"
 fi
