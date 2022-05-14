353f2a127a1032d76c1112b0efc2318e4e82937b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-14 17:09:11 +0800

    Update sync_images_1_26_7.sh

--- sync_images_1_26_7.sh
+++ sync_images_1_26_7.sh
@@ -5,18 +5,6 @@ T_REGISTRY="hbstarjason"
 
 IMAGES=$( cat images_list_1_26_7.yaml)
 
-for image in ${IMAGES}
-do 
-  echo ${IMAGES}
-  docker pull ${S_REGISTRY}/docker/${IMAGES}
-  docker tag ${S_REGISTRY}/docker/${IMAGES} ${T_REGISTRY}/${IMAGES}
-  #docker push ${T_REGISTRY}/${IMAGES}
-  docker images |grep ${S_REGISTRY}  
-done
-
-
-##################################################
-<<'COMMENT'
 cat images_list.yaml  | while read line
 do 
     echo ${line}
@@ -25,4 +13,8 @@ do
     #docker push ${T_REGISTRY}/${line}
     docker images |grep ${S_REGISTRY}
 done 
+
+##################################################
+<<'COMMENT'
+
 COMMENT
