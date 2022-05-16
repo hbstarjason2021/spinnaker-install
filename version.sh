b11a22eb5c38bd78a790be7911df24ffbdbdcb20
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-17 01:07:45 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -8,6 +8,8 @@ hal-v
 
 hal version list
 
+## https://storage.googleapis.com/halconfig/bom/1.26.7.yml
+
 ## https://spinnaker.io/community/releases/versions/ 
 SPINNAKER_VERSION=1.26.7
 hal version bom ${SPINNAKER_VERSION} -q -o yaml >/tmp/${SPINNAKER_VERSION}.yml
