9b254bee436e497106d9887a7021030f68ae8d33
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-21 00:43:53 +0800

    Update sync_images_1_26_7.sh

--- local_1_26_7/sync_images_1_26_7.sh
+++ local_1_26_7/sync_images_1_26_7.sh
@@ -6,12 +6,13 @@ T_REGISTRY="hbstarjason"
 cat images_list_1_26_7.yaml  | while read line
 do 
     echo ${line}
-    docker pull ${S_REGISTRY}/docker/${line}
-    docker tag ${S_REGISTRY}/docker/${line} ${T_REGISTRY}/${line}
-    docker push ${T_REGISTRY}/${line}
     
-    # docker pull ${T_REGISTRY}/${line}
-    # docker tag ${T_REGISTRY}/${line} ${S_REGISTRY}/docker/${line}
+    #docker pull ${S_REGISTRY}/docker/${line}
+    #docker tag ${S_REGISTRY}/docker/${line} ${T_REGISTRY}/${line}
+    #docker push ${T_REGISTRY}/${line}
+    
+    docker pull ${T_REGISTRY}/${line}
+    docker tag ${T_REGISTRY}/${line} ${S_REGISTRY}/docker/${line}
     docker images |grep ${S_REGISTRY}
     docker images |grep ${T_REGISTRY}
 done 
