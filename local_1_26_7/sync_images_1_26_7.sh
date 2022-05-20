455455124340373b370c0c751f90260fcd826a08
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-21 01:18:51 +0800

    Update sync_images_1_26_7.sh

--- local_1_26_7/sync_images_1_26_7.sh
+++ local_1_26_7/sync_images_1_26_7.sh
@@ -20,4 +20,22 @@ done
 ##################################################
 <<'COMMENT'
 
+images=(
+  clouddriver:8.0.4-20210625060028
+  deck:3.7.2-20210614020020
+  echo:2.17.1-20210429125836
+  fiat:1.16.0-20210422230020
+  front50:0.27.1-20210625161956
+  gate:1.22.2-20211214195244
+  orca:2.20.3-20210630022216
+  rosco:0.25.0-20210422230020
+  monitoring-daemon:0.19.3-20210415230021
+)
+
+for imageName in ${images[@]};do
+  docker pull ${T_REGISTRY}/$imageName
+  docker tag  ${T_REGISTRY}/$imageName ${S_REGISTRY}/$imageName
+  docker rmi  ${T_REGISTRY}/$imageName
+done
+
 COMMENT
