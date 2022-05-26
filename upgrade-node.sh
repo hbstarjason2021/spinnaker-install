270d55ab547d495d5f812189cfb410b9c54af058
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-26 21:50:08 +0800

    Update upgrade-node.sh

--- upgrade-node.sh
+++ upgrade-node.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=v1.19.4
+VERSION=1.19.4
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
