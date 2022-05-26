d6cf34a7c123b1295dc382f9b8a423eca9a4d003
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-26 20:56:21 +0800

    Update upgrade-master.sh

--- upgrade-master.sh
+++ upgrade-master.sh
@@ -11,7 +11,7 @@ set -e
 
 ### https://kubernetes.io/zh/docs/tasks/administer-cluster/kubeadm/kubeadm-upgrade/
 
-VERSION=1.19.16
+VERSION=1.19.4
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
