4c18e4c0869191c44916862d7d53da0ff6eb3598
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-05-26 18:21:36 +0800

    Update upgrade-node.sh

--- upgrade-node.sh
+++ upgrade-node.sh
@@ -2,17 +2,19 @@
 
 set -e
 
+VERSION=1.19.4
+
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 sudo apt-mark unhold kubeadm && \
-sudo apt-get update && sudo apt-get install -y kubeadm=1.19.4-00 && \
+sudo apt-get update && sudo apt-get install -y kubeadm=${VERSION}-00 && \
 sudo apt-mark hold kubeadm
 
 sudo kubeadm upgrade node
 
 sudo apt-mark unhold kubelet kubectl && \
-sudo apt-get update && sudo apt-get install -y kubelet=1.19.4-00 kubectl=1.19.4-00 && \
+sudo apt-get update && sudo apt-get install -y kubelet=${VERSION}-00 kubectl=${VERSION}-00 && \
 sudo apt-mark hold kubelet kubectl
 
 sudo systemctl daemon-reload && sudo systemctl restart kubelet
