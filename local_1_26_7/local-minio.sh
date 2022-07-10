f1f76a3e0fd1e478acd3d1ffb443690dede07b9d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-07-10 17:01:04 +0800

    Create local-minio.sh

---
+++ local_1_26_7/local-minio.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+### MINIO_ROOT_USER=$(< /dev/urandom tr -dc a-z | head -c${1:-4})
+### MINIO_ROOT_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8})
+
+MINIO_ROOT_USER=minioadmin
+MINIO_ROOT_PASSWORD=minioadmin
+MINIO_ENCRYPTION_KEY=minio-encryption-key
+LOCAL_IP=$(ifconfig ens3 |grep "inet "| awk '{print $2}')
+
+NAME=minio
+if [ -z "$(sudo docker ps -f name=${NAME} | grep ${NAME})" ]; then
+  echo -e "\033[32m Running a new minio container \033[0m"
+  ## echo "Running a new minio container"
+  sudo docker run -p 9001:9000 \
+    --name ${NAME} \
+    -d \
+    -e "MINIO_ROOT_USER_FILE=${MINIO_ROOT_USER}" \
+    -e "MINIO_ROOT_PASSWORD_FILE=${MINIO_ROOT_PASSWORD}" \
+    -e "MINIO_KMS_SECRET_KEY_FILE=minio-encryption-key:${MINIO_ENCRYPTION_KEY}" \
+    -v $HOME/minio/disk1:/disk1 \
+    -v $HOME/minio/disk2:/disk2 \
+    -v $HOME/minio/disk3:/disk3 \
+    -v $HOME/minio/disk4:/disk4 \
+    minio/minio server /disk{1...4}
+else
+  echo -e "\033[32m Starting minio container \033[0m"
+  ## echo "Starting minio container"
+  sudo docker start ${NAME}
+fi
+
+echo "$LOCAL_IP"
+
+echo ${MINIO_ROOT_PASSWORD} | hal config storage s3 edit \
+  --access-key-id ${MINIO_ROOT_USER} \
+  --secret-access-key \
+  --endpoint http://$LOCAL_IP:9001
